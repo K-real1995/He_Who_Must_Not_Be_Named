5e29dd7c80e8461e675337ff481a63f689ef7044
K-real1995 <77657168+K-real1995@users.noreply.github.com>
2021-11-23 20:00:28 +0700

    Delete Second_task.sql

--- Second_task.sql
+++
@@ -1,8 +0,0 @@
---Выбираем уникальные значения с применением фунции TIMEDIFF()
---Подсчитывающей разность между парами с id=2 до id=4 из таблицы 
---Timepair    
-SELECT DISTINCT TIMEDIFF( 
-                (SELECT end_pair FROM Timepair WHERE id=4),
-                (SELECT start_pair FROM Timepair WHERE id=2)
-                ) AS time
-FROM Timepair;
