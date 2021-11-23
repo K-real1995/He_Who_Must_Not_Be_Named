a4ee1e1ee707753dee760cff75ebbbc1948d6bfb
K-real1995 <77657168+K-real1995@users.noreply.github.com>
2021-11-23 20:00:35 +0700

    Delete Third_task.sql

--- Third_task.sql
+++
@@ -1,8 +0,0 @@
---Выводим уникальные значения из таблицы Rooms
-SELECT DISTINCT Rooms.*
-FROM Rooms
---Объединяя с таблицей Reservations
-JOIN Reservations
-    ON Rooms.id=Reservations.room_id
---Где колонка 'start_date' в графе недели равна 12 и год равен 2020
-WHERE WEEK(start_date, 1) = 12 AND YEAR(start_date)=2020;
